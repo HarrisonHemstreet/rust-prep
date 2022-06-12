d1b6e31ef13545f95c023d57af7f8693069a73c2
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2022-06-12 01:44:36 -0600

    leetcode; two_sum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    main::Solution::two_sum(vec![1, 2, 3, 4], 5);
+    main::Solution::two_sum(vec![1, 2, 3, 4], 4);
+    main::Solution::two_addends();
 }
 
 mod main {
@@ -27,6 +28,36 @@ mod main {
             println!("Not found, probably not working");
             vec![1, 2]
         }
+        // I am planning on using this function to figure out how I will get all the addends of a given number. From there, I can look for said combos within a given vector
+        pub fn two_addends() -> f32 {
+            let x: f32 = 5 as f32;
+            let y: f32 = x / 2.0;
+            let z: i32 = y.ceil() as i32;
+            let a: i32 = y.floor() as i32;
+
+            println!("z: {}, a: {}", z, a);
+            y
+            /*
+            let y: i32 = x.ceil() as i32;
+            let z: i32 = x.floor() as i32;
+            println!("y: {}, z: {}", y, z);
+            y
+            */
+            /*
+            let mut first: i32 = 0;
+            let mut second: i32 = 0;
+            let mut i: i32 = 0;
+            loop {
+                if i < num {
+                    println!("in here");
+                    i += 1;
+                    continue;
+                }
+                break;
+            }
+            vec![1, 2]
+            */
+        }
     }
 }
 /*
@@ -35,4 +66,12 @@ Given an array of integers nums and an integer target, return indices of the two
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
+
+
+steps:
+1. take the target and find all the two number combos equal to the target
+2. then search the vector for the numbers that make up the combos
+3. return answer
+
+
 */
