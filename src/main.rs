cbd741a0a7096a748285a34a0eaf4404c69817c4
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2022-06-16 00:27:46 -0600

    two_sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    main::Solution::two_sum(vec![1, 2, 3, 4], 4);
-    main::Solution::two_addends();
+    main::Solution::two_sum(vec![5, 9, 1, 2, 3, 4], 5);
 }
 
 mod main {
@@ -8,55 +7,28 @@ mod main {
     impl Solution {
         pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
             let mut i: usize = 0;
-            let mut _curr_sum: i32 = 0;
+            let mut j: usize = 0;
             loop {
-                if _curr_sum == target {
-                    println!(
-                        "returning: {:?}, {:?}",
-                        vec![nums[i - 1], nums[i]],
-                        vec![i - 1, i]
-                    );
-                    return vec![i as i32 - 1, i as i32];
-                }
-                if i < nums.len() - 1 {
-                    _curr_sum = nums[i] + nums[i + 1];
+                if i < nums.len() {
+                    let x: i32 = nums[i];
+                    let y: i32 = target - x;
+                    loop {
+                        if j < nums.len() {
+                            if y == nums[j] {
+                                return vec![i as i32, j as i32];
+                            }
+                            j += 1;
+                            continue;
+                        }
+                        j = 0;
+                        break;
+                    }
                     i += 1;
                     continue;
                 }
                 break;
             }
-            println!("Not found, probably not working");
-            vec![1, 2]
-        }
-        // I am planning on using this function to figure out how I will get all the addends of a given number. From there, I can look for said combos within a given vector
-        pub fn two_addends() -> f32 {
-            let x: f32 = 5 as f32;
-            let y: f32 = x / 2.0;
-            let z: i32 = y.ceil() as i32;
-            let a: i32 = y.floor() as i32;
-
-            println!("z: {}, a: {}", z, a);
-            y
-            /*
-            let y: i32 = x.ceil() as i32;
-            let z: i32 = x.floor() as i32;
-            println!("y: {}, z: {}", y, z);
-            y
-            */
-            /*
-            let mut first: i32 = 0;
-            let mut second: i32 = 0;
-            let mut i: i32 = 0;
-            loop {
-                if i < num {
-                    println!("in here");
-                    i += 1;
-                    continue;
-                }
-                break;
-            }
-            vec![1, 2]
-            */
+            vec![0, 0]
         }
     }
 }
